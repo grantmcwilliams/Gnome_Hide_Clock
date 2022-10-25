bfab193e469ea8d3b2a2dec217bb7db9fafb2f3b
Grant <grant@x1carbon.localnet.com>
2022-10-25 11:14:54 -0500

    New version for Gnome 43.

--- Hide_Clock@grantmcwilliams.com/extension.js
+++ Hide_Clock@grantmcwilliams.com/extension.js
@@ -1,23 +1,19 @@
 // Copyright (C) 2016 Grant McWilliams
 // Licence: GPLv2+
 
-let clockMenu = Panel.statusArea.dateMenu;
-let hiding;
+const Main = imports.ui.main;
+const Panel = Main.panel;
 
+let ClockMenu = Panel.statusArea.dateMenu;
 
 function enable() {
       clockMenu.container.hide();
-      hiding = clockMenu.actor.connect('notify::visible', actor => {
-        actor.hide()
-      })
 }
 
 function disable() {
-        if(hiding){
-                clockMenu.actor.disconnect(hiding);
-        }
         clockMenu.container.show();
 }
 
 function init() {
 }
+
